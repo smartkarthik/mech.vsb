ee2511d144717e442b97dfacf77bc7f5349a472f
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-02-20 16:17:47 +0530

    Create armstrong number.c

---
+++ armstrong number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    printf("enter the values");
+    scanf("%d",&i);
+    for(i=0;i<1;i++)
+        if(n*4==0)
+        {
+    printf("armstrong number");
+        }
+    else
+    {
+        printf("not armstrong");
+    }
+}

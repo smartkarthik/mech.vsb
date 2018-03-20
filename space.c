0e03bf0992031f3bace335b4679aa7fd8fa34e85
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-03-20 16:15:52 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[500];
+    int i=0,b=0;
+    printf("enter the character");
+    scanf("%[^\n]",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+    if(a[i]==' ')
+    b++;
+    }
+    printf("enter the sapce:%d",b++);
+    
+}
